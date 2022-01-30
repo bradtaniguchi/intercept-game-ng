645c08de290c07df4b0cf665a3cb094baa50c0ae
Brad Taniguchi <bradtaniguchi@gmail.com>
2022-01-30 12:05:05 -0800

    Clean up game-client-ng

--- apps/game-client-ng/src/app/app.component.ts
+++ apps/game-client-ng/src/app/app.component.ts
@@ -1,10 +1,9 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'nx-workspace-template-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppComponent {
-  title = 'game-client-ng';
-}
+export class AppComponent {}
