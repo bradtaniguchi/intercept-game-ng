b664d6f7a1220690d9450dc7ea6a67ee69622489
Brad Taniguchi <bradtaniguchi@gmail.com>
2022-02-06 13:13:00 -0800

    Add game-move-id spec

---
+++ libs/game/src/lib/models/game-session/game-move-id.spec.ts
@@ -0,0 +1,11 @@
+import { isGameMoveId } from './game-move-id';
+
+describe('isGameMoveId', () => {
+  test('returns true for valid game-move-id', () => {
+    expect(isGameMoveId('12345678')).toEqual(true);
+  });
+  test('returns false for invalid game-move-id', () => {
+    expect(isGameMoveId(12345678)).toEqual(false);
+    expect(isGameMoveId('some-string')).toEqual(false);
+  });
+});
