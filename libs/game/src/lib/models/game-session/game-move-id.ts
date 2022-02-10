2b841864a50083bf8d8d3e66445dd42115225eb1
Brad Taniguchi <bradtaniguchi@gmail.com>
2022-02-09 17:17:41 -0800

    Add game-move-id

--- libs/game/src/lib/models/game-session/game-move-id.ts
+++ libs/game/src/lib/models/game-session/game-move-id.ts
@@ -1,7 +1,9 @@
+import { nanoid } from 'nanoid';
+
 /**
  * The length of a game-session id.
  */
-export const GAME_MOVE_ID = 8;
+export const GAME_MOVE_ID_LENGTH = 8;
 
 /**
  * Type that represents a GameMoveId.
@@ -17,4 +19,10 @@ export const GameMoveId = (id: string): GameMoveId => id as GameMoveId;
  * Function that can be used to check if a value is a GameMoveId.
  */
 export const isGameMoveId = (id: unknown): id is GameMoveId =>
-  typeof id === 'string' && id.length === GAME_MOVE_ID;
+  typeof id === 'string' && id.length === GAME_MOVE_ID_LENGTH;
+
+/**
+ * Creates a valid game-move-id.
+ */
+export const createGameMoveId = (): GameMoveId =>
+  GameMoveId(nanoid(GAME_MOVE_ID_LENGTH));
